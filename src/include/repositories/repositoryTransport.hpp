9f0d1914b5c2d697aecc69dab83be578aa7f8f57
gili-julio <giliardoplay@gmail.com>
2024-08-10 03:32:02 -0300

    updates on repositoryTransport

--- src/include/repositories/repositoryTransport.hpp
+++ src/include/repositories/repositoryTransport.hpp
@@ -14,9 +14,9 @@ std::string transportTypeToString(TransportTypeEnum type);
 void createTableTransports(sqlite3* db);
 bool addTransportInTransports(sqlite3* db, const std::string& transportName, const std::string& type, const int capacity, const float speed,
     const float distanceBetweenRest, const float restTime);
-Transport* findTransportById(sqlite3* db, int transportId);
-bool removeTransportInTransports(sqlite3* db, const int transportId);
-bool editTransportInTransports(sqlite3* db, const int transportId, const Transport* newTransport);
+Transport* findTransportByName(sqlite3* db, const std::string& transportName);
+bool removeTransportInTransports(sqlite3* db, const std::string& transportName);
+bool editTransportInTransports(sqlite3* db, const std::string& transportName, const Transport* newTransport);
 bool listTransportInTransports(sqlite3* db, std::list<Transport>& transports);
 
 #endif
