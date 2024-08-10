6c9326d490014f04f92dbe976249ab83147310d8
gili-julio <giliardoplay@gmail.com>
2024-08-09 21:19:26 -0300

    updates on header of repositoryCity

--- src/include/repositories/repositoryCity.hpp
+++ src/include/repositories/repositoryCity.hpp
@@ -10,7 +10,7 @@ void createTableCities(sqlite3* db);
 bool addCityInCities(sqlite3* db, const std::string& cityName);
 City* findCityByName(sqlite3* db, const std::string& name);
 bool removeCityInCities(sqlite3* db, const std::string& cityName);
-bool editCityInCities(sqlite3* db, const std::string& cityName, const City* newCity)
+bool editCityInCities(sqlite3* db, const std::string& cityName, const City* newCity);
 bool listCityInCities(sqlite3* db, std::list<City>& cities);
 
 #endif
