817465d9a9754fe59879305af1002ced13936582
gili-julio <giliardoplay@gmail.com>
2024-08-09 21:53:59 -0300

    updates on repositoryTransport

--- src/impl/repositories/repositoryTransport.cpp
+++ src/impl/repositories/repositoryTransport.cpp
@@ -1,5 +1,26 @@
 #include "../../include/repositories/repositoryTransport.hpp"
 
+TransportTypeEnum stringToTransportType(const std::string& typeString) {
+    static const std::unordered_map<std::string, TransportTypeEnum> typeMap = {
+        {"GROUND", GROUND},
+        {"WATER", WATER}
+    };
+    
+    auto it = typeMap.find(typeString);
+    if (it != typeMap.end()) {
+        return it->second;
+    }
+    throw std::invalid_argument("Tipo de transporte inválido");
+}
+
+std::string transportTypeToString(TransportTypeEnum type) {
+    switch (type) {
+        case GROUND: return "GROUND";
+        case WATER: return "WATER";
+        default: throw std::invalid_argument("Tipo de transporte inválido");
+    }
+}
+
 void createTableTransports(sqlite3* db) {
     const char* sql_create = R"(
         CREATE TABLE IF NOT EXISTS transports (
@@ -74,7 +95,7 @@ Transport* findTransportById(sqlite3* db, int transportId) {
 
     if (sqlite3_step(stmt) == SQLITE_ROW) {
         std::string transportName(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
-        TransportTypeEnum type(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)));
+        std::string typeString(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)));
         int capacity = sqlite3_column_int(stmt, 2);
         float speed = static_cast<float>(sqlite3_column_double(stmt, 3));
         float distanceBetweenRest = static_cast<float>(sqlite3_column_double(stmt, 4));
@@ -82,7 +103,10 @@ Transport* findTransportById(sqlite3* db, int transportId) {
         float currentRestTime = static_cast<float>(sqlite3_column_double(stmt, 6));
         std::string currentPlace(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 7)));
 
-        transport = new Transport(transportName, type, capacity, speed, distanceBetweenRest, restTime, currentRestTime, currentPlace);
+        // Converte a string para TransportTypeEnum
+        TransportTypeEnum type = stringToTransportType(typeString);
+
+        transport = new Transport(transportName, type, capacity, speed, distanceBetweenRest, restTime, currentRestTime, findCityByName(db, currentPlace));
     }
 
     sqlite3_finalize(stmt);
@@ -108,7 +132,7 @@ bool removeTransportInTransports(sqlite3* db, const int transportId) {
         return false;
     }
 
-    if (sqlite3_bind_int(stmt, 1, transportName.c_str(), -1, SQLITE_STATIC) != SQLITE_OK) {
+    if (sqlite3_bind_int(stmt, 1, transportId) != SQLITE_OK) {
         std::cerr << "Erro ao vincular o nome do transporte: " << sqlite3_errmsg(db) << std::endl;
         sqlite3_finalize(stmt);
         return false;
@@ -142,14 +166,14 @@ bool editTransportInTransports(sqlite3* db, const int transportId, const Transpo
     }
 
     if (sqlite3_bind_text(stmt, 1, newTransport->getTransportName().c_str(), -1, SQLITE_STATIC) != SQLITE_OK ||
-        sqlite3_bind_text(stmt, 2, newTransport->getTransportType().c_str(), -1, SQLITE_STATIC) != SQLITE_OK ||
+        sqlite3_bind_text(stmt, 2, transportTypeToString(newTransport->getTransportType()).c_str(), -1, SQLITE_STATIC) != SQLITE_OK ||
         sqlite3_bind_int(stmt, 3, newTransport->getCapacity()) != SQLITE_OK ||
         sqlite3_bind_double(stmt, 4, newTransport->getSpeed()) != SQLITE_OK ||
         sqlite3_bind_double(stmt, 5, newTransport->getDistanceBetweenRest()) != SQLITE_OK ||
         sqlite3_bind_double(stmt, 6, newTransport->getRestTime()) != SQLITE_OK ||
         sqlite3_bind_double(stmt, 7, newTransport->getCurrentRestTime()) != SQLITE_OK ||
-        sqlite3_bind_text(stmt, 8, newTransport->getCurrentPlace()) != SQLITE_OK ||
-        sqlite3_bind_int(stmt, 9, transportId.c_str(), -1, SQLITE_STATIC) != SQLITE_OK) {
+        sqlite3_bind_text(stmt, 8, newTransport->getCurrentPlace()->getCityName().c_str(), -1, SQLITE_STATIC) != SQLITE_OK ||
+        sqlite3_bind_int(stmt, 9, transportId) != SQLITE_OK) {
 
         std::cerr << "Erro ao vincular os parâmetros: " << sqlite3_errmsg(db) << std::endl;
         sqlite3_finalize(stmt);
@@ -180,16 +204,19 @@ bool listTransportInTransports(sqlite3* db, std::list<Transport>& transports) {
     // Para cada linha retornada, os dados do transporte são recuperados e um objeto Transport é criado e adicionado à lista de transportes.
     while (sqlite3_step(stmt) == SQLITE_ROW) {
         std::string name(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
-        TransportTypeEnum type(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)));
+        std::string typeString(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)));
         int capacity = sqlite3_column_int(stmt, 2);
         float speed = static_cast<float>(sqlite3_column_double(stmt, 3));
         float distanceBetweenRest = static_cast<float>(sqlite3_column_double(stmt, 4));
         float restTime = static_cast<float>(sqlite3_column_double(stmt, 5));
         float currentRestTime = static_cast<float>(sqlite3_column_double(stmt, 6));
-        std::string currentPlace = static_cast<const char*>(sqlite3_column_text(stmt, 7));
+        std::string currentPlace(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 7)));
+
+        // Converte a string para TransportTypeEnum
+        TransportTypeEnum type = stringToTransportType(typeString);
 
         // Cria o objeto Transport e adiciona à lista
-        transports.emplace_back(name, type, capacity, speed, distanceBetweenRest, restTime, currentRestTime, currentPlace);
+        transports.emplace_back(name, type, capacity, speed, distanceBetweenRest, restTime, currentRestTime, findCityByName(db, currentPlace));
     }
 
     sqlite3_finalize(stmt);
--- src/include/repositories/repositoryTransport.hpp
+++ src/include/repositories/repositoryTransport.hpp
@@ -4,9 +4,13 @@
 #include "../../../sqlite3/sqlite3.h"
 #include "../enums/TransportTypeEnum.hpp"
 #include "../Transport.hpp"
+#include "./repositoryCity.hpp"
 #include <iostream>
 #include <list>
+#include <unordered_map>
 
+TransportTypeEnum stringToTransportType(const std::string& typeString);
+std::string transportTypeToString(TransportTypeEnum type);
 void createTableTransports(sqlite3* db);
 bool addTransportInTransports(sqlite3* db, const std::string& transportName, const std::string& type, const int capacity, const float speed,
     const float distanceBetweenRest, const float restTime);
